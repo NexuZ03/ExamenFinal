8fcb642ce05c1f8eae0c90eb96791c85be77a562
StrikerL1t4 <114685885+StrikerL1t4@users.noreply.github.com>
2022-11-30 13:55:31 -0500

    Historial de Envió
    Parte de Arturo

--- app/src/main/java/com/example/examenfinal/MainActivity.java
+++ app/src/main/java/com/example/examenfinal/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.examenfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button siguiente;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        siguiente=(Button)findViewById(R.id.siguiente);
+
+        siguiente.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Historial_de_Envios.class);
+                startActivity(i);
+            }
+        });
     }
 }
