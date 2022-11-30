94ce2d1a4d9b1f16b4132af6845828f51ee681fc
sebastian-cardenas <114688189+sebastian-cardenas@users.noreply.github.com>
2022-11-30 14:53:33 -0500

    parte 2 sebas
    historial de pedidos

---
+++ app/src/main/java/com/example/examenfinal/Historial_de_Pedididos.java
@@ -0,0 +1,19 @@
+package com.example.examenfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Historial_de_Pedididos extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historial_de_pedididos);
+
+
+    }
+}
--- app/src/main/java/com/example/examenfinal/MainActivity.java
+++ app/src/main/java/com/example/examenfinal/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.examenfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button eliminar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+    eliminar = (Button)findViewById(R.id.eliminar);
+
+        eliminar.setOnClickListener(new View.OnClickListener() {
+
+
+        @Override
+        public void onClick(View view) {
+            Intent i = new Intent(MainActivity.this,Historial_de_Pedididos.class);
+            startActivity(i);
+        }
+    });
+}
 }
