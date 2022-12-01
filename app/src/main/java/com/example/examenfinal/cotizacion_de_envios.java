fc36731386467a48fb3f720235e6671abb44c029
cuchitaa <96448235+cuchitaa@users.noreply.github.com>
2022-12-01 01:45:43 -0500

    ariana

--- app/src/main/java/com/example/examenfinal/cotizacion_de_envios.java
+++ app/src/main/java/com/example/examenfinal/cotizacion_de_envios.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -19,11 +21,21 @@ public class cotizacion_de_envios extends AppCompatActivity {
     ArrayList <String> arrayList_puno, arrayList_huancayo;
     ArrayAdapter <String> arrayAdapter_regi;
 
+    Double valordepa,pesito;
+
+
+    private EditText pesos;
+    private TextView resul;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cotizacion_de_envios);
+
+        pesos=(EditText)findViewById(R.id.peso);
+        resul=(TextView)findViewById(R.id.resul);
+
 
         //departamentos
         spinner_depa = (Spinner) findViewById(R.id.spinner_depa);
@@ -53,9 +65,11 @@ public class cotizacion_de_envios extends AppCompatActivity {
 
                 if (position==0){
                     arrayAdapter_regi=new ArrayAdapter<>(getApplicationContext(),android.R.layout.simple_spinner_item,arrayList_puno);
+                    valordepa=2.00;
                 }
                 if (position==1){
                     arrayAdapter_regi=new ArrayAdapter<>(getApplicationContext(),android.R.layout.simple_spinner_item,arrayList_huancayo);
+                    valordepa=3.00;
                 }
 
                 spinner_regi.setAdapter(arrayAdapter_regi);
@@ -70,7 +84,26 @@ public class cotizacion_de_envios extends AppCompatActivity {
 
 
 
+
+
+
     }
 
+    public void estimar (View view) {
+        String peso_String = pesos.getText().toString();
+        int peso_int=Integer.parseInt(peso_String);
+        if(peso_int>=0 && peso_int<=5){
+            pesito=3.0;
+        }
+        if(peso_int>6 && peso_int<=10){
+            pesito=5.0;
+        }
+        else {
+            pesito=10.0;
+        }
+        Double  total=(5.0+pesito+valordepa);
+
+
+    }
 
 }
