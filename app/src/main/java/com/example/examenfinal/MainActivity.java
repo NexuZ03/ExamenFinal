5e461ad7109a594bdf9eba98910c37eac35ff48e
NexuZ03 <paolomz579@gmail.com>
2022-12-15 04:26:30 -0500

    123

--- app/src/main/java/com/example/examenfinal/MainActivity.java
+++ app/src/main/java/com/example/examenfinal/MainActivity.java
@@ -1,28 +1,25 @@
 package com.example.examenfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+
 import android.widget.Button;
+import android.widget.EditText;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     Button buttonIngresar;
+    EditText editTextTextUser, editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buttonIngresar=(Button)findViewById(R.id.buttonIngresar);
+        buttonIngresar = (Button) findViewById(R.id.buttonIngresar);
+        editTextTextUser = findViewById(R.id.editTextTextUser);
+        editTextPassword = findViewById(R.id.editTextPassword);
 
-        buttonIngresar.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent i = new Intent(MainActivity.this, MenuInicio.class);
-                startActivity(i);
-            }
-        });
     }
 }
